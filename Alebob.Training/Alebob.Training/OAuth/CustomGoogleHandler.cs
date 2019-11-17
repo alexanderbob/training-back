55f2150db6f9a8a4ac35fd517aaff3d5a871be27
Alexander Bobryshev <alexanderbob@live.com>
2019-11-18 02:04:09 +0400

    Completely fixed kestrel + nginx OAuth2 redirections issue

--- Alebob.Training/Alebob.Training/OAuth/CustomGoogleHandler.cs
+++ Alebob.Training/Alebob.Training/OAuth/CustomGoogleHandler.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.Google;
+using Microsoft.AspNetCore.Authentication.OAuth;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
@@ -7,11 +8,19 @@ using Microsoft.Extensions.Options;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Text.Encodings.Web;
+using System.Text.Json;
 using System.Threading.Tasks;
 
 namespace Alebob.Training.OAuth
 {
+    /// <summary>
+    /// We are using kestel on HTTP with nginx as proxy server on HTTPS.
+    /// Unfortunately, BuildRedirectUri puts HTTP as a Scheme, because request was proxied by nginx.
+    /// With this class, we are fixing this issue for non-Development builds
+    /// </summary>
     public class CustomGoogleHandler : GoogleHandler
     {
         private bool isProduction;
@@ -20,10 +29,50 @@ namespace Alebob.Training.OAuth
             isProduction = !env.IsDevelopment();
         }
 
-        // we are using kestel on HTTP with nginx as proxy server on HTTPS
-        // unfortunately, BuildRedirectUri puts http as Scheme, because
-        // request was proxied by nginx
         protected override string BuildChallengeUrl(AuthenticationProperties properties, string redirectUri)
+        {
+            redirectUri = RebuildRedirectUri(redirectUri);
+            Logger.LogWarning($"Sending ChallengeUrl with redirectUri {redirectUri}");
+            return base.BuildChallengeUrl(properties, redirectUri);
+        }
+
+        protected override async Task<OAuthTokenResponse> ExchangeCodeAsync(OAuthCodeExchangeContext context)
+        {
+            string redirectUri = RebuildRedirectUri(context.RedirectUri);
+            var tokenRequestParameters = new Dictionary<string, string>()
+            {
+                { "client_id", Options.ClientId },
+                { "redirect_uri", redirectUri },
+                { "client_secret", Options.ClientSecret },
+                { "code", context.Code },
+                { "grant_type", "authorization_code" },
+            };
+
+            // PKCE https://tools.ietf.org/html/rfc7636#section-4.5, see BuildChallengeUrl
+            if (context.Properties.Items.TryGetValue(OAuthConstants.CodeVerifierKey, out var codeVerifier))
+            {
+                tokenRequestParameters.Add(OAuthConstants.CodeVerifierKey, codeVerifier);
+                context.Properties.Items.Remove(OAuthConstants.CodeVerifierKey);
+            }
+            var requestContent = new FormUrlEncodedContent(tokenRequestParameters);
+            Logger.LogInformation($"Sending requestContent to get token: {requestContent}");
+            var requestMessage = new HttpRequestMessage(HttpMethod.Post, Options.TokenEndpoint);
+            requestMessage.Headers.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            requestMessage.Content = requestContent;
+            var response = await Backchannel.SendAsync(requestMessage, Context.RequestAborted);
+            if (response.IsSuccessStatusCode)
+            {
+                var payload = JsonDocument.Parse(await response.Content.ReadAsStringAsync());
+                return OAuthTokenResponse.Success(payload);
+            }
+            else
+            {
+                var error = "OAuth token endpoint failure: " + await response.Content.ReadAsStringAsync();
+                return OAuthTokenResponse.Failed(new Exception(error));
+            }
+        }
+
+        private string RebuildRedirectUri(string redirectUri)
         {
             if (isProduction)
             {
@@ -32,7 +81,7 @@ namespace Alebob.Training.OAuth
                 ub.Port = -1;
                 redirectUri = ub.ToString();
             }
-            return base.BuildChallengeUrl(properties, redirectUri);
+            return redirectUri;
         }
     }
 }
