da5b5d1b4a701f340018c3ec406287021cead4b5
Alexander Bobryshev <alexanderbob@live.com>
2019-11-19 02:33:51 +0400

    Bandaided exercises controller

--- Alebob.Training/Alebob.Training/Controllers/ExercisesController.cs
+++ Alebob.Training/Alebob.Training/Controllers/ExercisesController.cs
@@ -20,18 +20,11 @@ namespace Alebob.Training.Controllers
         private ILogger<ExercisesController> _logger;
         private IHistoryProvider _historyProvider;
         private IExerciseProvider _exerciseProvider;
-        private Task<Dictionary<string, DataLayer.Models.ExerciseMetadata>> _availableExercises;
         public ExercisesController(ILogger<ExercisesController> logger, IHistoryProvider dataProvider, IExerciseProvider exerciseProvider)
         {
             _logger = logger ?? throw new ArgumentNullException(nameof(logger));
             _historyProvider = dataProvider ?? throw new ArgumentNullException(nameof(dataProvider));
             _exerciseProvider = exerciseProvider ?? throw new ArgumentNullException(nameof(exerciseProvider));
-#pragma warning disable CA2008 // Do not create tasks without passing a TaskScheduler
-            _exerciseProvider.GetExercises().ContinueWith(_ =>
-            {
-                _availableExercises = Task.FromResult(_.Result.ToDictionary(k => k.Code));
-            });
-#pragma warning restore CA2008 // Do not create tasks without passing a TaskScheduler
         }
 
         [HttpGet("/api/[controller]/list")]
@@ -47,8 +40,9 @@ namespace Alebob.Training.Controllers
             try
             {
                 var key = new TrainingDayKey(User.FindFirst(Claims.UserId).Value, isoDate);
-                var exercises = (await _availableExercises.ConfigureAwait(false))
-                    .ToDictionary(x => x.Value.Code, y => y.Value.AsViewModel());
+                //TODO: cache exercises
+                var exercises = (await _exerciseProvider.GetExercises().ConfigureAwait(false))
+                    .ToDictionary(x => x.Code, y => y.AsViewModel());
                 var entry = await _historyProvider.GetEntry(key).ConfigureAwait(false);
                 return Ok(entry.AsViewModel(exercises));
             }
@@ -73,8 +67,9 @@ namespace Alebob.Training.Controllers
             {
                 var key = new TrainingDayKey(User.FindFirst(Claims.UserId).Value, isoDate);
                 var result = await _historyProvider.UpsertExercise(key, exerciseCode, setsData.Select(x => x.AsDataModel())).ConfigureAwait(false);
-                
-                var exercises = await _availableExercises.ConfigureAwait(false);
+
+                var exercises = (await _exerciseProvider.GetExercises().ConfigureAwait(false))
+                    .ToDictionary(x => x.Code, y => y.AsViewModel());
                 var training = await _historyProvider.GetEntry(key).ConfigureAwait(false);
                 var description = string.Join(", ", exercises.Where(x => training.Exercises.ContainsKey(x.Key)).Select(x => x.Value.Description));
                 await _historyProvider.UpdateEntry(key, training.Name, description).ConfigureAwait(false);
